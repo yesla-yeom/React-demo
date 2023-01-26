cb882bf1b16073e325e3dd49fdbffa9f889f25b7
예나나 <111760323+yesla-yeom@users.noreply.github.com>
2023-01-26 19:23:21 +0900

    23.1.26. added filter.js .css

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -8,7 +8,7 @@ const NewExenpse = (props) => {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    props.onAddExpense(expenseData);
+    props.onAddExpense(expenseData); // 프롭스 전달은 상향식 (자식컴포넌트에서 부모컴포넌트로) && 이게 App.js로 전달
   };
 
   return (
